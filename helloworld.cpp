7e8dabca21ab9b36ef507093f231ce824c37ce81
nponder17 <hq3276@wayne.edu>
2025-09-07 16:55:00 -0400

    First commit for assignment 1

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,3 +1,4 @@
+
 // Simple C++ program to display "Hello World"
 // Header file for input output functions
 #include<iostream>
@@ -9,4 +10,11 @@ int main()
 // prints hello world
 cout<<"Hello World!";
 return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
 }
